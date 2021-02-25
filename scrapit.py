9b63b2be3cdde8669523bcc7cfcab4d8959254c3
JoeCare <Joe Care>
2021-02-25 10:38:20 +0100

    Putting crumbs into funcs

--- scrapit.py
+++ scrapit.py
@@ -1,35 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-# print("price:", next(price.stripped_strings).replace(" ", ""))
 
 # noinspection SpellCheckingInspection
-html_dom = requests.get(
+html_main = requests.get(
 	'https://www.otodom.pl/wynajem/mieszkanie/warszawa/?search%5Bfilter_'
 	'float_price%3Ato%5D=1750&search%5Bcity_id%5D=26&page=2')
-print(html_dom.text)
-bs = BeautifulSoup(html_dom.content, 'html.parser')
-print(bs)
+bs = BeautifulSoup(html_main.content, 'html.parser')
+# raw_content = bs.find_all('div', class_="listing")
 offers = bs.find_all('div', class_="offer-item-details")
-listing = bs.find_all('div', class_="listing")
-pagination = bs.find('ul', class_="pager")
 
-# def list_the_offer
-for lis in listing:
-	offers = lis.find_all('div', class_="offer-item-details")
-	# pagination = bs.find('ul', class_="pager")
-	for offer in offers:
-		# offer = offer.prettify()
-		print("\n!!!!!OFFER!!!!!!:")
+
+def list_offers(res_set):
+	"""
+	Scraps relevant data and order in list of offers
+	:param parsed_soup:
+	:return:
+	"""
+	i = 1
+	for offer in res_set:
+
 		link = offer.find('a')['href']
-		print("link:", link)  # ['href']
-		size = offer.find('strong', class_="visible-xs-block")
-		print("size:", size.text)
+		print("link:", link, end='\n')
+
 		item_title = offer.find('span', class_="offer-item-title")
-		print("name:", item_title.text)
+		print(f" {i}. OFFER:", f'"{item_title.text}"', sep='\n')
 		# description = offer.find('span', class_="hidden-xs")
 		description = offer.find('p', class_="text-nowrap")
-		print("description:", description.text)
+		print("Short info:", description.text)
 		params = offer.find('ul', class_="params")
 		# for attr in params:
 		#     print("attr:", attr)
@@ -41,9 +39,14 @@ for lis in listing:
 		# print("price:", prize, price, prices)
 		# price2 = pri
 		print("price:", price.text.replace(" ", "").replace("\n", ""))
+		print("price:", next(price.stripped_strings).replace(" ", ""))
+
+		size = offer.find('strong', class_="visible-xs-block")
+		print("size:", size.text)
+		i += 1
 
 
-def change_page(parsed_soup):
+def paginate(parsed_soup):
 	"""
 	Returns dictionary for pagination mapping
 	May be rewritten for a generator to invoke pagination by next() method
@@ -58,4 +61,6 @@ def change_page(parsed_soup):
 	return {"current": current_page, "previous": prev_page, "next": next_page}
 
 
-print(change_page(bs))
+# print(paginate(bs)['current'])
+
+print(list_offers(offers))
